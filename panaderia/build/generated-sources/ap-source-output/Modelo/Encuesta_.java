dd4d0c1b94ca769596e3f7acec9e871f1c520414
Cirigod <siehart1392@gmail.com>
2020-01-07 22:06:19 -0600

    casi todas las vistas
    a darle we

--- panaderia/build/generated-sources/ap-source-output/Modelo/Encuesta_.java
+++ panaderia/build/generated-sources/ap-source-output/Modelo/Encuesta_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T12:40:24")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T14:17:26")
 @StaticMetamodel(Encuesta.class)
 public class Encuesta_ { 
 
